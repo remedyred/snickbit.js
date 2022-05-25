75d88364b553792cbeed46a65aa02bc6ebe4cde9
snickbit <nicklowe@snickbit.com>
2022-05-24 22:53:36 -0400

    fix: type definitions

--- packages/controller/src/index.ts
+++ packages/controller/src/index.ts
@@ -1,56 +1,55 @@
 import mitt, {Handler} from 'mitt'
 import {objectClone} from '@snickbit/utilities'
 
-export type ControllerKey = string | symbol
-export type ControllerValue = any
-
 export type WatchStop = () => void
 export type Watchers = Record<string, WatchStop>
 
-export class Controller<State> {
-	protected state = {} as State
-	protected originalState = {} as State
-	protected proxy: Controller<State>
+export class Controller<T extends object = any, D = Partial<T>> {
+	protected state: T
+	protected originalState: T
+	protected proxy: Controller<T>
 	protected emitter = mitt()
-	public $state: ProxyHandler<Controller<State>> & State
+	public $state: ProxyHandler<Controller<T>> & T
 
 	persistable: string[] = []
 
 	protected id = (...keys: string[]) => ['controller', this.$name, ...keys].join('.')
 
-	constructor(data: Partial<State> = {}) {
-		this.originalState = objectClone(data) as State
+	constructor(data: D = {} as D) {
+		this.originalState = objectClone(data) as T
+		const state = {} as D
 		for (let key in data) {
-			this.state[key] = data[key]
+			state[key] = data[key]
 		}
+		this.state = state as unknown as T
 
 		this.proxy = new Proxy(this, {
-			get(target: Controller<State>, prop: string, receiver?: any): any {
+			get(target: Controller<T>, prop: string, receiver?: any): any {
 				if (prop in target) {
 					return target[prop]
 				}
 
 				if (target.$has(prop)) {
-					return target.$get(prop)
+					return target.$get(prop as keyof T)
 				}
 
 				return Reflect.get(target, prop, receiver)
 			},
-			set: function (target: Controller<State>, prop: string, value?: any) {
-				target.$set(prop, value)
+			set: function (target: Controller<T>, prop: string, value?: any) {
+				target.$set(prop as keyof T, value)
 				return true
 			}
 		})
 
 		this.$state = new Proxy(this.state as any, {
-			get: (target: Controller<State>, prop: string) => {
+			get: (target: Controller<T>, prop: string) => {
 				if (this.$has(prop)) {
-					return this.$get(prop)
+					return this.$get(prop as keyof T)
 				}
 				return undefined
 			},
-			set: (target: Controller<State>, prop: string, value: any) => {
-				this.$set(prop, value)
+			set: (target: Controller<T>, prop: string, value: any) => {
+				this.$set(prop as keyof T, value)
 				return true
 			}
 		})
@@ -62,15 +61,15 @@ export class Controller<State> {
 		return this.constructor.name
 	}
 
-	$get(key: ControllerKey) {
+	$get(key: keyof T) {
 		return this.state[key]
 	}
 
-	$set(key: ControllerKey, value: ControllerValue) {
-		this.state[key] = value
+	$set(key: keyof T | keyof D, value: any) {
+		this.state[key as keyof T] = value
 	}
 
-	$has(key: ControllerKey) {
+	$has(key: string) {
 		return key in this.state
 	}
 
@@ -78,7 +77,7 @@ export class Controller<State> {
 		return Object.keys(this.state)
 	}
 
-	$patch(data: State) {
+	$patch(data: D) {
 		for (let key in data) {
 			this.$set(key, data[key])
 		}
