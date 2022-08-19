4fab6a4cd928f08ff73a856178ef77821a0f60f8
Snickbit <snickbit@users.noreply.github.com>
2022-08-19 17:59:16 -0400

    refactor(tests): use toBe instead of toEqual

--- packages/utilities/__tests__/numbers.test.ts
+++ packages/utilities/__tests__/numbers.test.ts
@@ -2,30 +2,30 @@ import {numberPad} from '../src'
 
 describe('numbers', () => {
 	it('numberPad(5) should return 05', () => {
-		expect(numberPad(5)).toEqual('05')
+		expect(numberPad(5)).toBe('05')
 	})
 
 	it('numberPad(5, 2) should return 05', () => {
-		expect(numberPad(5, 2)).toEqual('05')
+		expect(numberPad(5, 2)).toBe('05')
 	})
 
 	it('numberPad(50, 2) should return 50', () => {
-		expect(numberPad(50, 2)).toEqual('50')
+		expect(numberPad(50, 2)).toBe('50')
 	})
 
 	it('numberPad(500, 2) should return 500', () => {
-		expect(numberPad(500, 2)).toEqual('500')
+		expect(numberPad(500, 2)).toBe('500')
 	})
 
 	it('numberPad(5, 3) should return 005', () => {
-		expect(numberPad(5, 3)).toEqual('005')
+		expect(numberPad(5, 3)).toBe('005')
 	})
 
 	it('numberPad(50, 3) should return 050', () => {
-		expect(numberPad(50, 3)).toEqual('050')
+		expect(numberPad(50, 3)).toBe('050')
 	})
 
 	it('numberPad(500, 3) should return 500', () => {
-		expect(numberPad(500, 3)).toEqual('500')
+		expect(numberPad(500, 3)).toBe('500')
 	})
 })
