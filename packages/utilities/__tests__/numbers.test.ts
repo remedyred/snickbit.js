9502db0d52ae46b80d67c1c7900794dafa675114
Snickbit <snickbit@users.noreply.github.com>
2022-08-19 18:11:17 -0400

    refactor(tests): put numberPad tests inside of a describe

--- packages/utilities/__tests__/numbers.test.ts
+++ packages/utilities/__tests__/numbers.test.ts
@@ -1,31 +1,32 @@
 import {numberPad} from '../src'
 
 describe('numbers', () => {
-	it('numberPad(5) should return 05', () => {
-		expect(numberPad(5)).toBe('05')
-	})
+	describe('numberPad', () => {
+		it('(5) should return 05', () => {
+			expect(numberPad(5)).toBe('05')
+		})
+		it('(5, 2) should return 05', () => {
+			expect(numberPad(5, 2)).toBe('05')
+		})
 
-	it('numberPad(5, 2) should return 05', () => {
-		expect(numberPad(5, 2)).toBe('05')
-	})
+		it('(50, 2) should return 50', () => {
+			expect(numberPad(50, 2)).toBe('50')
+		})
 
-	it('numberPad(50, 2) should return 50', () => {
-		expect(numberPad(50, 2)).toBe('50')
-	})
+		it('(500, 2) should return 500', () => {
+			expect(numberPad(500, 2)).toBe('500')
+		})
 
-	it('numberPad(500, 2) should return 500', () => {
-		expect(numberPad(500, 2)).toBe('500')
-	})
+		it('(5, 3) should return 005', () => {
+			expect(numberPad(5, 3)).toBe('005')
+		})
 
-	it('numberPad(5, 3) should return 005', () => {
-		expect(numberPad(5, 3)).toBe('005')
-	})
+		it('(50, 3) should return 050', () => {
+			expect(numberPad(50, 3)).toBe('050')
+		})
 
-	it('numberPad(50, 3) should return 050', () => {
-		expect(numberPad(50, 3)).toBe('050')
-	})
-
-	it('numberPad(500, 3) should return 500', () => {
-		expect(numberPad(500, 3)).toBe('500')
+		it('(500, 3) should return 500', () => {
+			expect(numberPad(500, 3)).toBe('500')
+		})
 	})
 })
