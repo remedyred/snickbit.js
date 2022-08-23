12c732c86bae1fa7701d677cb6100ab5a0928b74
Snickbit <snickbit@users.noreply.github.com>
2022-08-22 23:48:43 -0400

    feat(node-utilities): replace options.recursive with recursive

---
+++ packages/plural/__fixtures__/singular.ts
@@ -0,0 +1,9 @@
+export default [
+	['dingo', 'dingos'],
+	['mango', 'mangoes'],
+	['echo', 'echos'],
+	['ghetto', 'ghettoes'],
+	['nucleus', 'nucleuses'],
+	['bureau', 'bureaux'],
+	['seraph', 'seraphs']
+]
