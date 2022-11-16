092a7ac30d7734cf194a78265c9325f16745de43
Snickbit <snickbit@users.noreply.github.com>
2022-11-16 09:51:23 -0500

    chore(release): publish (#107)

---
+++ packages/state/src/factory.ts
@@ -0,0 +1,10 @@
+import {Store, StoreOptions} from './Store'
+
+const _states: Record<string, any> = {}
+export type UseStore<T extends object = any> = () => Store<T>
+
+export function createStore<State extends object = any>(hydration: State = {} as State, options?: Partial<StoreOptions>): UseStore<State> {
+	const state = new Store<State>(hydration, options)
+	_states[state.$id] = state
+	return () => _states[state.$id]
+}
