e5496786eaa30876625b3824c7f1fde7ca94f9b1
Snickbit <snickbit@users.noreply.github.com>
2022-11-19 17:20:23 -0500

    fix: correct store type

--- packages/state/src/factory.ts
+++ packages/state/src/factory.ts
@@ -1,7 +1,7 @@
 import {Store, StoreOptions} from './Store'
 
 const _states: Record<string, any> = {}
-export type UseStore<T extends object = any> = () => Store<T>
+export type UseStore<T extends object = any> = () => Store<T> & T
 
 export function createStore<State extends object = any>(hydration: State = {} as State, options?: Partial<StoreOptions>): UseStore<State> {
 	const state = new Store<State>(hydration, options)
