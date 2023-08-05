7136760f6f3e4674888aabb508a091e424c034fd
Snickbit <snickbit@users.noreply.github.com>
2023-08-05 13:15:39 -0400

    chore(plural): migrate to individual repo

--- packages/plural/__fixtures__/plural.ts
+++
@@ -1,9 +0,0 @@
-export default [
-	['plateaux', 'plateaux'],
-	['axis', 'axes'],
-	['basis', 'bases'],
-	['automatum', 'automata'],
-	['thou', 'you'],
-	['axiS', 'axeS'],
-	['passerby', 'passersby']
-]
--- packages/plural/__fixtures__/singular.ts
+++
@@ -1,9 +0,0 @@
-export default [
-	['dingo', 'dingos'],
-	['mango', 'mangoes'],
-	['echo', 'echos'],
-	['ghetto', 'ghettoes'],
-	['nucleus', 'nucleuses'],
-	['bureau', 'bureaux'],
-	['seraph', 'seraphs']
-]
