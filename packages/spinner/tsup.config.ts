7a7453b2a5e4005c5f648ef885142d346548fffd
Snickbit <snickbit@users.noreply.github.com>
2023-08-07 12:09:21 -0400

    build: generate indexes with tsup/esbuild plugin

---
+++ packages/spinner/tsup.config.ts
@@ -0,0 +1,10 @@
+import {defineConfig} from 'tsup'
+import {indexerPlugin} from '@snickbit/indexer'
+
+export default defineConfig({
+	entry: ['src/index.ts'],
+	clean: true,
+	dts: true,
+	format: ['esm', 'cjs'],
+	esbuildPlugins: [indexerPlugin()]
+})
